74ad88ada4a8111e039dab72a7efdd797d7e45a8
li-deepa <li.deepa02@gmail.com>
2021-12-19 23:35:17 -0800

    rebooting python

---
+++ variable.py
@@ -0,0 +1,8 @@
+# varaiables save information in its address so it is easy to reference the value using 
+# the name of variable
+#naming coventions for variables
+# varaiables start with _ or chearacter or uppercase and 
+# no space between varaiables and 
+# we can concatenate the variables
+name="ram"
+print(name+" is a good boy")
